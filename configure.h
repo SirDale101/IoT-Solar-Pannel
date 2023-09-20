ecf7270491f196bbc320dfa648fadd90b9446a00
SirDale <134013549+SirDale101@users.noreply.github.com>
2023-09-20 22:55:21 +0100

    Add files via upload

---
+++ configure.h
@@ -0,0 +1,5 @@
+static const char iotConnStr[] = "HostName=iotc-cc677808-26f0-48d0-b58d-a0f7a9c5324e.azure-devices.net;DeviceId=28ufbti0or5;SharedAccessKey=Trt/QiMkHMZKLKBw1hZ8SVvdbvGvRr19cQR8lKXO+rU=";
+
+// Wi-Fi information
+static const char wifi_ssid[] = "<VALUE>";
+static const char wifi_password[] = "<VALUE>";
